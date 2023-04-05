7775a69a8362fd7eb4e402065251cba803b10710
wiramaharddhika <wiramaharddhika@gmail.com>
2023-04-05 23:34:36 +0800

    feat: whitelist some source in content security policy header

--- next.config.js
+++ next.config.js
@@ -8,7 +8,11 @@ module.exports = {
       {
         source: "/",
         headers: [
-          { key: "Content-Security-Policy", value: "default-src 'self'" },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' https://fonts.googleapis.com https://static1.plasmic.app; img-src 'self' data:",
+          },
           { key: "X-Frame-Options", value: "SAMEORIGIN" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "same-site" },
