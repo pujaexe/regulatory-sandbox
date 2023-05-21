f8c67de9c0162ce5e7e5462bb5e7db2a6f0bcf89
puja.exe <puja.exe@gmail.com>
2023-05-21 12:35:25 +0800

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ import {
 } from "@plasmicapp/react-web";
 
 interface ButtonProps extends DefaultButtonProps {
-  target?: boolean;
+  target?: HTMLAttributeAnchorTarget | undefined;
   // Feel free to add any additional props that this component should receive
 }
 function Button_(props: ButtonProps, ref: ButtonRef) {
