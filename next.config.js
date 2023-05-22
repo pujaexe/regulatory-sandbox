6d32545c0d757c1d60651a2d900c17e0908ecbc6
puja.exe <puja.exe@gmail.com>
2023-05-22 13:14:47 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   trailingSlash: true,
   async headers() {
     return [
